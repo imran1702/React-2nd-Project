88cc5af3082116e2c310254632062af86fda05bc
MD. Imran Hossain <imranakash2020@gmail.com>
2025-07-03 03:31:01 +0600

    json

--- src/components/slices/countSlice.js
+++ src/components/slices/countSlice.js
@@ -3,18 +3,16 @@ import { createSlice } from '@reduxjs/toolkit'
 export const countSlice = createSlice({
   name: 'counter',
   initialState: {
-    value: 0
+    value: JSON.parse(localStorage.getItem("cartItemCount")) || 0
   },
   reducers: {
     increment: state => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1
+      localStorage.setItem("cartItemCount", JSON.stringify(state.value))
     },
     decrement: state => {
       state.value -= 1
+      localStorage.setItem("cartItemCount", JSON.stringify(state.value))
     },
     // incrementByAmount: (state, action) => {
     //   state.value += action.payload
